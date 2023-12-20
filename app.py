a6dead5e85f2eba8aec74c4c09ec33f3d9148eaa
jayasriveeramallu <95433055+jayasriveeramallu@users.noreply.github.com>
2023-12-20 08:13:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 from flask import Flask, request, jsonify
-
 import assemblyai as aai
-import sounddevice as sd
-import numpy as np
-import io
-import wave
 import tempfile
+import os 
 
 app = Flask(__name__)
 from dotenv import load_dotenv
 load_dotenv()
-import os 
+
 # Set your AssemblyAI API key
-aai.settings.api_key=os.getenv('API_KEY')
-# Set your AssemblyAI API key
+aai.settings.api_key = os.getenv('API_KEY')
 
 transcriber = aai.Transcriber()
 
-@app.route('/transcribe', methods=['POST'])
+@app.route('/', methods=['POST'])
 def transcribe():
     if request.method == 'POST':
         # Get raw audio data from the POST request
@@ -29,11 +24,8 @@ def transcribe():
         # Save audio data to a temporary WAV file
         with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio_file:
             audio_path = temp_audio_file.name
-            with wave.open(audio_path, 'wb') as wf:
-                wf.setnchannels(1)
-                wf.setsampwidth(2)
-                wf.setframerate(fs)
-                wf.writeframes(audio_data)
+            with open(audio_path, 'wb') as af:
+                af.write(audio_data)
 
         # Transcribe audio from the temporary file
         transcript = transcriber.transcribe(audio_path)
@@ -43,4 +35,3 @@ def transcribe():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
